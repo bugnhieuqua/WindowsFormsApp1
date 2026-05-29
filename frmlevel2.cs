d3c44a58be3d2084671b34897c07f2616fa6e2b3
nhuc9499-ai <nhuc9499@gmail.com>
2026-05-29 09:36:53 +0700

    Cow upfile

---
+++ frmlevel2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmlevel2 : Form
+    {
+        public frmlevel2()
+        {
+            InitializeComponent();
+        }
+
+        private void ptrb2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl28_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
